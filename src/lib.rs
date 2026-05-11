c5103484a35e57c004d254d4dbc62e4df404c7bf
share121 <me@s121.top>
2026-05-11 20:05:36 +0800

    feat: 初始化项目
    Signed-off-by: share121 <me@s121.top>

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use bytes::BytesMut;
+use std::cell::Cell;
+
+mod local_buf {
+    use super::*;
+
+    thread_local! {
+        static POOL: Cell<Option<BytesMut>> = const { Cell::new(None) };
+    }
+
+    pub(super) fn clear_buffer() -> Option<BytesMut> {
+        POOL.with(|cell| cell.take())
+    }
+
+    pub(super) fn take_buffer(capacity: usize) -> BytesMut {
+        POOL.with(|cell| {
+            if let Some(mut buf) = cell.take() {
+                if buf.capacity() >= capacity {
+                    buf.clear();
+                    return buf;
+                }
+                cell.set(Some(buf));
+            }
+            #[cfg(feature = "tracing")]
+            tracing::debug!("current thread LocalBuf is None, allocate {capacity}B");
+            BytesMut::with_capacity(capacity)
+        })
+    }
+
+    pub(super) fn return_buffer(buf: BytesMut) {
+        POOL.with(|cell| {
+            let mut slot = cell.take();
+            if slot.as_ref().map(|b| b.capacity()).unwrap_or(0) < buf.capacity() {
+                slot = Some(buf);
+            }
+            cell.set(slot);
+        })
+    }
+}
+
+/// 支持多线程异步的线程本地缓冲区
+pub struct LocalBuf {
+    inner: Option<BytesMut>,
+}
+
+impl LocalBuf {
+    pub fn with_capacity(capacity: usize) -> Self {
+        let buf = local_buf::take_buffer(capacity);
+        Self { inner: Some(buf) }
+    }
+
+    pub fn from_bytes(buf: BytesMut) -> Self {
+        Self { inner: Some(buf) }
+    }
+
+    /// 调用 `into_bytes` 后，你需要调用 `from_bytes` 手动放回缓冲区，否则缓冲区就会被正常释放，无法复用
+    pub fn into_bytes(mut self) -> BytesMut {
+        self.inner.take().expect("LocalBuf 已被意外消费")
+    }
+
+    /// 清理当前线程的缓冲区
+    pub fn clear_buffer() -> Option<BytesMut> {
+        local_buf::clear_buffer()
+    }
+}
+
+impl std::ops::Deref for LocalBuf {
+    type Target = BytesMut;
+
+    fn deref(&self) -> &Self::Target {
+        self.inner.as_ref().expect("LocalBuf 已被意外消费")
+    }
+}
+
+impl std::ops::DerefMut for LocalBuf {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.inner.as_mut().expect("LocalBuf 已被意外消费")
+    }
+}
+
+impl Drop for LocalBuf {
+    fn drop(&mut self) {
+        if let Some(buf) = self.inner.take() {
+            local_buf::return_buffer(buf);
+        }
+    }
+}
+
+impl From<BytesMut> for LocalBuf {
+    fn from(value: BytesMut) -> Self {
+        Self::from_bytes(value)
+    }
+}
